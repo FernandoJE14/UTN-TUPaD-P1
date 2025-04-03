e40807e0ccfc85912601ab57329ae84180633e74
Fernando JE <javier.fernando97@gmail.com>
2025-04-02 22:01:38 -0300

    cambios

--- 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
+++ 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
@@ -1,1 +1,63 @@
-print("hola")
+# 1)
+# edad = int(input("Ingrese su edad: "))
+
+# if edad >= 18:
+#     print("Es mayor de edad")
+
+# 2)
+# nota = int(input("Ingrese la nota: "))
+
+# if nota >= 6:
+#     print("Aprobado")
+# else:
+#     print("Desaprobado")
+
+# 3)     
+# num = int(input("Ingrese un numero par: "))
+
+# if num % 2 == 0:
+#     print("Ha ingresado un numero par")
+# else:
+#     print("Por favor, ingrese un numero par")             
+
+# 4)
+# edad = int(input("Ingrese su edad: "))
+
+# if edad < 12:
+#     print("Niño/a")
+# elif edad >= 12 and edad < 18:
+#     print("Adolescente")
+# elif edad >= 18 and edad < 30:
+#     print("Adulto/a joven")
+# else:
+#     print("Adulto/a")        
+
+# 5)
+# clave = input("Ingrese una contraseña de entre 8 y 14 caracteres: ")
+
+# if len(clave) >= 8 and len(clave) <= 14:
+#     print("Ha ingresado una contraseña correcta")
+# else:
+#     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+
+# 6)
+# from statistics import mode, mean, median
+# import random
+# numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
+
+# moda = mode(numeros_aleatorios)
+# media = mean(numeros_aleatorios)
+# mediana = median(numeros_aleatorios)
+
+# if (media > mediana) and (mediana < moda):
+#     print("Hay sesgo positivo")
+# elif (media < mediana) and (mediana < moda):
+#     print("Hay sesgo negativo")
+# elif media == moda == mediana:
+#     print("Sin sesgo")
+# else:
+#     pass
+
+# 6)
+
+
