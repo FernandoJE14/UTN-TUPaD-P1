db9659cc30165402c04b9aad345a55296079e715
Fernando JE <javier.fernando97@gmail.com>
2025-04-03 17:33:27 -0300

    cambios

--- 03 Estructuras Condicionales/ELICHIRIBEHETY_TP-03.py
+++ 03 Estructuras Condicionales/ELICHIRIBEHETY_TP-03.py
@@ -1,10 +1,12 @@
 # 1)
+
 # edad = int(input("Ingrese su edad: "))
 
 # if edad >= 18:
 #     print("Es mayor de edad")
 
 # 2)
+
 # nota = int(input("Ingrese la nota: "))
 
 # if nota >= 6:
@@ -12,7 +14,8 @@
 # else:
 #     print("Desaprobado")
 
-# 3)     
+# 3)
+      
 # num = int(input("Ingrese un numero par: "))
 
 # if num % 2 == 0:
@@ -21,6 +24,7 @@
 #     print("Por favor, ingrese un numero par")             
 
 # 4)
+
 # edad = int(input("Ingrese su edad: "))
 
 # if edad < 12:
@@ -33,6 +37,7 @@
 #     print("Adulto/a")        
 
 # 5)
+
 # clave = input("Ingrese una contraseña de entre 8 y 14 caracteres: ")
 
 # if len(clave) >= 8 and len(clave) <= 14:
@@ -41,6 +46,7 @@
 #     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
 
 # 6)
+
 # from statistics import mode, mean, median
 # import random
 # numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
@@ -58,6 +64,44 @@
 # else:
 #     pass
 
-# 6)
+# 7)
+
+# string = input("Ingrese una frase o palabra: ")
+# ultima_letra = string[-1].lower()
+
+# if ultima_letra in ("a", "e", "i", "o", "u"):
+#     print(f"{string} !")
+# else:
+#     print(string)
+
+# 8)
+
+# nombre = input("Ingrese su nombre: ")
+# opcion = int(input("Ingrese opcion 1 (mayusculas), 2 (minusculas) o 3 (primera letra mayuscula): "))
+
+# if opcion == 1:
+#     print(nombre.upper())
+# elif opcion == 2:
+#     print(nombre.lower())
+# else:
+#     print(nombre.title())
+
+# 9)
+
+# num = int(input("Ingrese la magnitud del terremoto: "))
+
+# if num < 3:
+#     print("Muy leve (imperceptible)")
+# elif num >= 3 and num < 4:
+#     print("Leve (ligeramente perceptible)")   
+# elif num >= 4 and num < 5:
+#     print("Moderado (sentido por personas, pero generalmente no causa daños)")
+# elif num >= 5 and num < 6:
+#     print("Fuerte (puede causar daños en estructuras débiles)") 
+# elif num >= 6 and num < 7:
+#     print("Muy Fuerte (puede causar daños significativos)")   
+# else:
+#     print("Extremo (puede causar graves daños a gran escala)") 
+
 
 
